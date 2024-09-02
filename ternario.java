8febf4193a54ec84515a563a1c94ebde40fa6021
Guiromaos <guiromaodasilva@gmail.com>
2024-09-02 09:10:43 -0300

    controle de fluxo

---
+++ ternario.java
@@ -0,0 +1,8 @@
+
+public class ternario {
+        public static void main(String[] args) {
+            int nota = 6;
+            string resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Recuperação";
+            System.out.println(resultado);
+        }    
+}
