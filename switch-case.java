e9345ca665b389969c546a4f62a132ee4e7c80b3
Guiromaos <guiromaodasilva@gmail.com>
2024-09-02 11:05:52 -0300

    switch case

---
+++ switch-case.java
@@ -0,0 +1,21 @@
+public class switch-case{
+    public static void main(String[] args) {
+
+        String plano = "T";
+
+        switch(plano) {
+            case "T":{
+                system.out.println("5gb youtube");
+            }
+            case "M": {
+                system.out.println("whats e insta gratis");
+            }
+            case "B": {
+                system.out.println("100 minutos de ligação");
+            }
+        }
+
+    }
+
+
+}
